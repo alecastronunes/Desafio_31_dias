6e85b9db752a252ba70c7e00372d19d4688b3b0e
alecastronunes <alexandre.castro92@gmail.com>
2025-01-10 18:09:10 -0300

    Enviando arquivo do primeiro exercício, referente ao dia 2 do desafio

---
+++ Dia02/exe_01.js
@@ -0,0 +1,11 @@
+// Escreva um código que exiba "Bom dia", "Boa tarde" ou "Boa noite" com base em um valor da variável.
+
+let hora = 14;
+
+if (hora < 12) {
+  console.log("Bom dia!!!");
+} else if (hora < 18) {
+  console.log("Boa tarde!!!");
+} else {
+  console.log("Boa noite!!!");
+}
