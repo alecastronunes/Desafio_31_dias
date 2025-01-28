7d2877aabd06230cc271c4087683e1e4241b1fd3
alecastronunes <alexandre.castro92@gmail.com>
2025-01-28 15:20:08 -0300

    Enviando exercício 02, referente ao dia 02 do desafio

---
+++ Dia02/exe_02.js
@@ -0,0 +1,44 @@
+// Use um switch para exibir o nome de um mês com base em um número (1 para Janeiro, 2 para Fevereiro, etc.).
+
+let nomeMes = 9
+
+switch(nomeMes) {
+    case 1:
+        console.log("Janeiro");
+        break;
+    case 2:
+        console.log("Fevereiro");
+        break;
+    case 3: 
+        console.log("Março");
+        break;
+    case 4:
+        console.log("Abril");
+        break;
+    case 5:
+        console.log("Maio");
+        break;
+    case 6: 
+        console.log("Junho");
+        break;
+    case 7:
+        console.log("Julho");
+        break;
+    case 8:
+        console.log("Agosto");
+        break;
+    case 9:
+        console.log("Setembo");
+        break;
+    case 10:
+        console.log("Outubro");
+        break;
+    case 11:
+        console.log("Novembro");
+        break;
+    case 12:
+        console.log("Dezembro");
+        break;
+    default: 
+        console.log("Mês não encontrado!!!");
+}
